bb88a666221cc6c5024682291c130151d33e71a5
vans8800 <muwang86@126.com>
2024-12-24 11:02:39 +0800

    Test Pytorch CUDA Environment

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import sys
+
+sys.path.append('D:\\Python312\\Lib\\site-packages')
+
+import torch
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print_hi(torch.cuda.is_available())
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
